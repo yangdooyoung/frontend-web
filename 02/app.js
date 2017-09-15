482b53a7fb5f92814d55d587c95a0a49e77e3362
yangdooyoung <31643986+yangdooyoung@users.noreply.github.com>
2017-09-15 18:26:36 +0900

    adding files

---
+++ 02/app.js
@@ -0,0 +1,34 @@
+// JavaScript source code
+
+//var str = 'hello world';
+//console.log(str);
+
+//var isMan = true;
+//var log = function (str) {
+  //  console.log(str);
+//};
+//function log(str) {
+  //  console.log(str);
+//}
+
+//image = []; // new Array();
+
+//image[0] = '1.jpg';
+//image[1] = '2.jpg';
+
+//image = ["1.jpg", 2, { name: "3.jpg" }];
+
+//if (isMan) {
+//    console.log('남자다')
+//}
+
+
+//else {
+//    console.log('여자다')
+//}
+
+//var person = {
+//    name:'aji',
+//    age: 11,
+//
+//}
---
+++ 02/q1.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+//1부터100출력
+for (var i = 1; i < 101; i++) { 
+        console.log(i); 
+    } 
+
+//짝수출력
+for (var i = 1; i < 101; i++) { 
+ if (i%2 == 0) { 
+       console.log(i); 
+  } 
+ } 
