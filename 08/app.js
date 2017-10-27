38258b9cb2064909c29199cf75ac1df048973061
yangdooyoung <31643986+yangdooyoung@users.noreply.github.com>
2017-10-27 18:17:08 +0900

    Add files via upload

---
+++ 08/app.js
@@ -0,0 +1,21 @@
+//$('div').css('background-color','black');
+
+
+//$('div').removeClass('blue');
+//console.log($('div').height())
+
+
+//$('div').height(200)
+
+//console.log(document.querySelector('img').src)
+
+//$('div').html('<h2>div</h2>')
+
+//var div $('div').addClass('blue');
+//$div.html('<h2>div</h2>')
+
+
+
+//$('#btn').on('click',function(){
+//  $('div').animate({:200}, 1000);
+  //})
